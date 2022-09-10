3a3474de1ea80e158c338f48ec717547a4c7af06
Suvam subedi <91051249+suvamsubedi@users.noreply.github.com>
2022-09-10 15:42:51 +0545

    leaflet-map

--- src/App.js
+++ src/App.js
@@ -1,87 +1,90 @@
-import React, {Component} from 'react';
-import './App.css';
-import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import leafGreen from './assets/leaf-green.png';
-import leafRed from './assets/leaf-red.png';
-import leafOrange from './assets/leaf-orange.png';
-import leafShadow from './assets/leaf-shadow.png';
-
-
+import React, { Component } from "react";
+import "./App.css";
+import L from "leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import leafGreen from "./assets/leaf-green.png";
+import leafRed from "./assets/leaf-red.png";
+import leafOrange from "./assets/leaf-orange.png";
 
 class App extends Component {
-  
   state = {
     greenIcon: {
-      lat: 35.787449,
-      lng: -78.6438197,
+      lat: 28.3949,
+      lng: 84.124,
     },
     redIcon: {
-      lat: 35.774416,
-      lng: -78.633271,
+      lat: 28.384,
+      lng: 83.225,
     },
     orangeIcon: {
-      lat: 35.772790,
-      lng: -78.652305,
+      lat: 28.3947,
+      lng: 82.332,
     },
-    zoom: 13
-  }
-
+    zoom: 7,
+  };
 
   grenIcon = L.icon({
     iconUrl: leafGreen,
-    shadowUrl: leafShadow,
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76]
+
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76],
   });
 
   redIcon = L.icon({
     iconUrl: leafRed,
-    shadowUrl: leafShadow,
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -86]
+
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -86],
   });
 
   orangeIcon = L.icon({
     iconUrl: leafOrange,
-    shadowUrl: leafShadow,
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -86]
+
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -86],
   });
 
-  render(){
+  render() {
     const positionRedIcon = [this.state.redIcon.lat, this.state.redIcon.lng];
-    const positionGreenIcon = [this.state.greenIcon.lat, this.state.greenIcon.lng];
-    const positionOrangeIcon = [this.state.orangeIcon.lat, this.state.orangeIcon.lng];
+    const positionGreenIcon = [
+      this.state.greenIcon.lat,
+      this.state.greenIcon.lng,
+    ];
+    const positionOrangeIcon = [
+      this.state.orangeIcon.lat,
+      this.state.orangeIcon.lng,
+    ];
     return (
       <Map className="map" center={positionGreenIcon} zoom={this.state.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://vectortile.naxa.com.np/federal/province.mvt/?tile={z}/{x}/{y}.png"
+        />
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
         <Marker position={positionGreenIcon} icon={this.grenIcon}>
-          <Popup>
-          I am a green leaf
-          </Popup>
+          <Popup>provinence Layer</Popup>
         </Marker>
         <Marker position={positionRedIcon} icon={this.redIcon}>
-          <Popup>
-          I am a red leaf
-          </Popup>
+          <Popup>District Layer</Popup>
         </Marker>
         <Marker position={positionOrangeIcon} icon={this.orangeIcon}>
-          <Popup>
-          I am an orange leaf
-          </Popup>
+          <Popup>Municipality Layer</Popup>
         </Marker>
       </Map>
     );
